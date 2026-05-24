3c189124f91f7aeed51fcb5f619ed129a7841933
hu-qi <huqi1024@gmail.com>
2026-05-24 18:56:40 +0800

    fix: update png.

--- src/assets/defaultMural.ts
+++ src/assets/defaultMural.ts
@@ -1,4 +1,4 @@
-import defaultMuralUrl from './defaultMural.png'
+import defaultMuralUrl from './defaultMural.jpg'
 
 export const DEFAULT_MURAL_NAME = 'Jiuselu mural demo'
 export const DEFAULT_MURAL_URL = defaultMuralUrl
